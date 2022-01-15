17b368c084e740a16b3e4d2525ce7c0a704a8517
kuzminykh <geve95@gmail.com>
2022-01-15 22:26:59 +0500

    lesson_176933 / Работа со спринг

---
+++ src/main/java/ru/geekbrains/spring_mini/models/Student.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.spring_mini.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "age")
+    private Integer age;
+}
